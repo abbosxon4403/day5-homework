b574e68f55178decd405dbee740e83b5b0c86d3a
abbosxon <abbosxon.oripov@yandex.ru>
2022-04-03 16:16:11 +0500

    add ages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let age = Math.round(Math.random() * 100);
+console.log(age);
+if ((age > 6 && age <= 17)) {
+    console.log("Maktab o'quvchisi");
+} else if ((age > 17 && age <= 24)) {
+    (console.log("Universitet yoshi"));
+} else if ((age > 25 && age <= 40)) {
+    console.log("Ishlash yoshi");
+} else if ((age > 40)) {
+    console.log("Nuroniylar yoshi");
+}
